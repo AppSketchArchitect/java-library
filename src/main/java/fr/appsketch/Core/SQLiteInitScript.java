69f07c848f1521e1ed5770b68ada8e5669bf449a
Kyrohe <enzoromoli44@gmail.com>
2026-01-16 16:44:25 +0100

    * Add HibernateManager

---
+++ src/main/java/fr/appsketch/Core/SQLiteInitScript.java
@@ -0,0 +1,22 @@
+package fr.appsketch.Core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SQLiteInitScript {
+    private static final Logger log = LoggerFactory.getLogger(SQLiteInitScript.class);
+
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:database.db";
+
+        try (Connection ignored = DriverManager.getConnection(url)) {
+            System.out.println("Base SQLite et table créées avec succès !");
+
+        } catch (Exception e) {
+            log.error("Database Exception: ", e);
+        }
+    }
+}
